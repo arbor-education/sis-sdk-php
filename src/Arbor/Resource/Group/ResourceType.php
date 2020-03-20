0d5306993e8307efdbd32da650874f1eca9a0ad5
Djordje Milojevic <djordje.milojevic@arbor-education.com>
2020-03-20 14:52:45 +0100

    Removed all Group Models from API

--- src/Arbor/Resource/Group/ResourceType.php
+++ src/Arbor/Resource/Group/ResourceType.php
@@ -4,23 +4,4 @@ namespace Arbor\Resource\Group;
 
 class ResourceType
 {
-	const GROUP_INSTITUTION = 'Group_Institution';
-	const GROUP_OPERATOR = 'Group_Operator';
-	const GROUP_KPI_TARGET = 'Group_KpiTarget';
-	const GROUP_KPI_INSTITUTION_TARGET = 'Group_KpiInstitutionTarget';
-	const GROUP_INSTITUTION_PREDECESSOR = 'Group_InstitutionPredecessor';
-	const GROUP_INSTITUTION_CURRICULUM_TIER_ASSIGNMENT = 'Group_InstitutionCurriculumTierAssignment';
-	const GROUP_INSTITUTION_TYPE_ASSIGNMENT = 'Group_InstitutionTypeAssignment';
-	const GROUP_SCHOOL_PHASE_ASSIGNMENT = 'Group_SchoolPhaseAssignment';
-	const GROUP_EDUCATIONAL_INSTITUTION = 'Group_EducationalInstitution';
-	const GROUP_INSTITUTION_DATA_SHARING_REQUEST = 'Group_InstitutionDataSharingRequest';
-	const GROUP_INSTITUTION_COMMUNICATION = 'Group_InstitutionCommunication';
-	const GROUP_ASSESSMENT = 'Group_Assessment';
-	const GROUP_ASSESSMENT_ASPECT = 'Group_AssessmentAspect';
-	const GROUP_ASSESSMENT_DERIVATION = 'Group_AssessmentDerivation';
-	const GROUP_ASSESSMENT_DERIVATION_DEPENDANT = 'Group_AssessmentDerivationDependant';
-	const GROUP_ASSESSMENT_GRADE_SET = 'Group_AssessmentGradeSet';
-	const GROUP_GRADE_POINT_SCALE = 'Group_GradePointScale';
-	const GROUP_GRADE_SET = 'Group_GradeSet';
-	const GROUP_GRADE = 'Group_Grade';
 }
