bdac4caca5bed31f2a9a803f3f848992630b4705
Nemanja Andrejevic <andrejevicnemanja@gmail.com>
2025-05-16 18:00:58 +0200

    MIG-14813: Switching Guzzle with PSR compliant http client

--- examples/example-bootstrap.php
+++ examples/example-bootstrap.php
@@ -4,11 +4,28 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $config = require __DIR__ . '/config.php';
 
-$api = new \Arbor\Api\Gateway\RestGateway(
+$httpClient = new \Arbor\Api\Gateway\HttpClient(
+    null,
+    null,
+    null,
     $config['api']['baseUrl'],
     $config['api']['auth']['user'],
     $config['api']['auth']['password']
 );
+
+$api = new \Arbor\Api\Gateway\PsrRestGateway(
+    $httpClient,
+    new \Arbor\Model\Hydrator(),
+    new \Arbor\Filter\CamelCaseToDash(),
+    new \Arbor\Filter\PluralizeFilter(),
+);
+
+//
+//$api = new \Arbor\Api\Gateway\RestGateway(
+//    $config['api']['baseUrl'],
+//    $config['api']['auth']['user'],
+//    $config['api']['auth']['password']
+//);
 
 \Arbor\Model\ModelBase::setDefaultGateway($api);
 
