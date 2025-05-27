b64e64609cdd5eb8f2a659aee277f1e07232dcd0
Nemanja Andrejevic <andrejevicnemanja@gmail.com>
2025-05-27 17:40:24 +0200

    MIG-14813: Adding more tests

--- tests/Unit/Arbor/Api/Gateway/PsrRestGatewayTest.php
+++ tests/Unit/Arbor/Api/Gateway/PsrRestGatewayTest.php
@@ -7,17 +7,24 @@ namespace Arbor\Test\Unit\Arbor\Api\Gateway;
 use Arbor\Api\Exception\ServerErrorException;
 use Arbor\Api\Gateway\PsrRestGateway;
 use Arbor\Api\Gateway\HttpClient\HttpClientInterface;
+use Arbor\ChangeLog\Change;
 use Arbor\Filter\CamelCaseToDash;
 use Arbor\Filter\PluralizeFilter;
+use Arbor\Model\Collection;
 use Arbor\Model\Hydrator;
 use Arbor\Model\ModelBase;
+use Arbor\Model\Staff;
+use Arbor\Query\Query;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
 use ReflectionException;
 
 #[CoversClass(PsrRestGateway::class)]
+#[CoversClass(Change::class)]
+#[CoversClass(Query::class)]
 class PsrRestGatewayTest extends TestCase
 {
     private PsrRestGateway $gateway;
@@ -179,5 +186,419 @@ class PsrRestGatewayTest extends TestCase
             ->willReturn([]); // No resourceRoot key
 
         $this->gateway->refresh($model);
+    }
+
+    /**
+     * @throws ServerErrorException
+     * @throws \Arbor\Model\Exception
+     * @throws \Arbor\Query\Exception
+     */
+    public function testRetrieveReturnsHydratedModel()
+    {
+        $resource = 'Staff';
+        $id = '123';
+        $resourceRoot = 'staff';
+        $url = '/rest-v2/staff/123';
+        $arrayRepresentation = [
+            $resourceRoot => ['id' => $id, 'name' => 'Test Name']
+        ];
+
+        $this->camelCaseToDash->method('filter')->willReturn('staff');
+        $this->pluralizeFilter->method('filter')->willReturn('Staff');
+        $this->httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->with(HttpClientInterface::HTTP_METHOD_GET, $url)
+            ->willReturn($arrayRepresentation);
+
+        $this->hydrator->expects($this->once())->method('hydrateModel');
+
+        $result = $this->gateway->retrieve($resource, $id);
+
+        $this->assertInstanceOf(ModelBase::class, $result);
+        $this->assertInstanceOf(Staff::class, $result);
+    }
+
+    #[DataProvider('modelInstancesDataProvider')]
+    public function testInstantiateModelReturnsCorrectModelInstance($modelType)
+    {
+        $model = $this->gateway->instantiateModel($modelType);
+        $this->assertInstanceOf(ModelBase::class, $model);
+        $this->assertEquals($modelType, $model->getResourceType());
+    }
+
+    public static function modelInstancesDataProvider(): array
+    {
+        return [
+            ['Student'], // Test with a resource type that exists
+            ['Module_Resource'], // Test with a module resource type (simulate file does not exist)
+            ['NonExistentResource'] // Test with a resource type that does not exist (should fall back to ModelBase)
+        ];
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testUpdateReturnsUpdatedModel()
+    {
+        $model = $this->createMock(ModelBase::class);
+        $model->method('getResourceUrl')->willReturn('/rest-v2/resource/1');
+        $model->method('getResourceType')->willReturn('Resource');
+        $model->method('getResourceId')->willReturn('1');
+
+        $this->hydrator->method('extractArray')->willReturn(['foo' => 'bar']);
+        $this->hydrator->expects($this->atLeastOnce())->method('hydrateModel');
+
+        $this->httpClient->method('sendRequest')->willReturnOnConsecutiveCalls(
+            ['resource' => ['foo' => 'baz']], // retrieve
+            ['resource' => ['foo' => 'bar']]  // update
+        );
+
+        $this->pluralizeFilter->method('filter')->willReturn('Resources');
+
+        $this->camelCaseToDash->method('filter')->willReturn('resources');
+
+        $result = $this->gateway->update($model);
+
+        $this->assertSame($model, $result);
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testUpdateReturnsNewModel()
+    {
+        $model = $this->createMock(ModelBase::class);
+        $model->method('getResourceUrl')->willReturn('/rest-v2/resource/1');
+        $model->method('getResourceType')->willReturn('Resource');
+        $model->method('getResourceId')->willReturn('1');
+
+        $this->hydrator->method('extractArray')->willReturnOnConsecutiveCalls(
+            [['foo' => 'bar']],
+            [['foo' => 'bar2']]
+        );
+        $this->hydrator->expects($this->atLeastOnce())->method('hydrateModel');
+
+        $this->httpClient->method('sendRequest')->willReturnOnConsecutiveCalls(
+            ['resource' => ['foo' => 'baz']], // retrieve
+            ['resource' => ['foo' => 'bar2']]  // update
+        );
+
+        $this->pluralizeFilter->method('filter')->willReturn('Resources');
+
+        $this->camelCaseToDash->method('filter')->willReturn('resources');
+
+        $result = $this->gateway->update($model);
+
+        $this->assertSame($model, $result);
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testUpdateReturnsNewModelWithRevisionId()
+    {
+        $model = $this->createMock(ModelBase::class);
+        $model->method('getResourceUrl')->willReturn('/rest-v2/resource/1');
+        $model->method('getResourceType')->willReturn('Resource');
+        $model->method('getResourceId')->willReturn('1');
+
+        $this->hydrator->method('extractArray')->willReturnOnConsecutiveCalls(
+            [['foo' => 'bar']],
+            [['foo' => 'bar2']]
+        );
+        $this->hydrator->expects($this->atLeastOnce())->method('hydrateModel');
+
+        $this->httpClient->method('sendRequest')->willReturnOnConsecutiveCalls(
+            ['resource' => ['foo' => 'baz']], // retrieve
+            ['resource' => ['foo' => 'bar2'], 'revisionId' => 1]  // update
+        );
+
+        $this->pluralizeFilter->method('filter')->willReturn('Resources');
+
+        $this->camelCaseToDash->method('filter')->willReturn('resources');
+
+        $result = $this->gateway->update($model);
+
+        $this->assertSame($model, $result);
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testThrowExceptionUpdateModelWithoutResource()
+    {
+        $model = $this->createMock(ModelBase::class);
+        $model->method('getResourceUrl')->willReturn('/rest-v2/resource/1');
+        $model->method('getResourceType')->willReturn('Resource');
+        $model->method('getResourceId')->willReturn('1');
+
+        $this->hydrator->method('extractArray')->willReturnOnConsecutiveCalls(
+            [['foo' => 'bar']],
+            []
+        );
+        $this->hydrator->expects($this->atLeastOnce())->method('hydrateModel');
+
+        $this->httpClient->method('sendRequest')->willReturnOnConsecutiveCalls(
+            ['resource' => ['foo' => 'baz']], // retrieve
+            []  // update
+        );
+
+        $this->pluralizeFilter->method('filter')->willReturn('Resources');
+
+        $this->camelCaseToDash->method('filter')->willReturn('resources');
+
+        $this->expectException(\Arbor\Query\Exception::class);
+        $this->gateway->update($model);
+    }
+
+    /**
+     * @throws ServerErrorException
+     * @throws \Arbor\Model\Exception
+     */
+    public function testGetChangesReturnsArrayOfChangeObjects(): void
+    {
+        $resourceType = 'Student';
+        $fromRevision = 10;
+        $toRevision = 20;
+
+        $this->pluralizeFilter->method('filter')->willReturn('Students');
+        $this->camelCaseToDash->method('filter')->willReturn('Students');
+
+        $this->httpClient->method('sendRequest')
+            ->willReturn([
+                'changes' => [
+                    [
+                        'changedObject' => ['id' => 1],
+                        'changeType' => 'update',
+                        'fromRevision' => 10,
+                        'toRevision' => 11
+                    ]
+                ]
+            ]);
+
+        $result = $this->gateway->getChanges($resourceType, $fromRevision, $toRevision);
+
+        $this->assertIsArray($result);
+        $this->assertInstanceOf(Change::class, $result[0]);
+    }
+
+    /**
+     * @throws ServerErrorException
+     * @throws \Arbor\Model\Exception
+     */
+    public function testGetChangesReturnsWithoutChangesArray(): void
+    {
+        $resourceType = 'Student';
+        $fromRevision = 10;
+        $toRevision = 20;
+
+        $this->pluralizeFilter->method('filter')->willReturn('Students');
+        $this->camelCaseToDash->method('filter')->willReturn('Students');
+
+        $this->httpClient->method('sendRequest')->willReturn([]);
+
+        $result = $this->gateway->getChanges($resourceType, $fromRevision, $toRevision);
+
+        $this->assertIsArray($result);
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testQueryReturnsHydratedModelWhenResourceExists(): void
+    {
+        // Create a Query mock
+        $query = $this->createMock(Query::class);
+        $resourceType = 'Student';
+        $pluralizedResourceType = 'Students';
+        $queryOptions = ['param1' => 'value1'];
+        $resourceRoot = 'students';
+        $url = '/rest-v2/Students';
+
+        // Set up expectations for the Query mock
+        $query->expects($this->atLeastOnce())
+            ->method('getResourceType')
+            ->willReturn($resourceType);
+
+        $query->expects($this->once())
+            ->method('getQueryOptions')
+            ->willReturn($queryOptions);
+
+        // Set up expectations for PluralizeFilter
+        $this->pluralizeFilter->expects($this->once())
+            ->method('filter')
+            ->with($resourceType)
+            ->willReturn($pluralizedResourceType);
+
+        // Set up expectations for HttpClient
+        $options = ['query' => $queryOptions];
+        $responseData = [$resourceRoot => [['id' => '123']]];
+        $this->httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->with(HttpClientInterface::HTTP_METHOD_GET, $url, $options)
+            ->willReturn($responseData);
+
+        // Call the method under test
+        $result = $this->gateway->query($query);
+
+        // Assert the result
+        $this->assertInstanceOf(Collection::class, $result);
+        $student = $result->offsetGet('0');
+        $this->assertInstanceOf(ModelBase::class, $student);
+        $this->assertSame('Student', $student->getResourceType());
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testQueryReturnsEmptyCollectionWhenResourceDoesNotExist(): void
+    {
+        // Create a Query mock
+        $query = $this->createMock(Query::class);
+        $resourceType = 'Student';
+        $pluralizedResourceType = 'Students';
+        $queryOptions = ['param1' => 'value1'];
+        $url = '/rest-v2/Students';
+
+        // Set up expectations for the Query mock
+        $query->expects($this->atLeastOnce())
+            ->method('getResourceType')
+            ->willReturn($resourceType);
+
+        $query->expects($this->once())
+            ->method('getQueryOptions')
+            ->willReturn($queryOptions);
+
+        // Set up expectations for PluralizeFilter
+        $this->pluralizeFilter->expects($this->once())
+            ->method('filter')
+            ->with($resourceType)
+            ->willReturn($pluralizedResourceType);
+
+        // Set up expectations for HttpClient
+        $options = ['query' => $queryOptions];
+        $responseData = []; // Empty response without the resource root
+        $this->httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->with(HttpClientInterface::HTTP_METHOD_GET, $url, $options)
+            ->willReturn($responseData);
+
+        // Call the method under test
+        $result = $this->gateway->query($query);
+
+        // Assert the result
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertCount(0, $result);
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testFindAllReturnsArrayOfModels()
+    {
+        $resourceType = 'Student';
+        $propertyFilters = ['name' => 'John'];
+        $userTags = ['tag' => 'value'];
+        $url = '/rest-v2/Students';
+        $options = [ 'query' => ['filters.name.equals' => 'John']];
+        $responseData = ['students' => [['id' => '123']]];
+
+        $mockCollection = $this->createMock(Collection::class);
+        $mockCollection->method('getArrayCopy')->willReturn(['model1', 'model2']);
+
+        $this->camelCaseToDash->method('filter')->willReturn('students');
+        $this->pluralizeFilter->method('filter')->willReturn('Students');
+        $this->httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->with(HttpClientInterface::HTTP_METHOD_GET, $url, $options)
+            ->willReturn($responseData);
+
+        $result = $this->gateway->findAll($resourceType, $propertyFilters, $userTags);
+
+        $this->assertIsArray($result);
+        $student = $result['0'];
+        $this->assertInstanceOf(ModelBase::class, $student);
+        $this->assertSame('Student', $student->getResourceType());
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testFindAllReturnsArrayOfModelsWithOperators()
+    {
+        $resourceType = 'Student';
+        $propertyFilters = ['lastName' => 'Smith', 'name' => ['OR', 'John']];
+        $userTags = ['tag' => 'value'];
+        $url = '/rest-v2/Students';
+        $options = [ 'query' => [
+            'filters.name.OR' => 'John',
+            'filters.lastName.equals' => 'Smith'
+        ]];
+        $responseData = ['students' => [['id' => '123']]];
+
+        $mockCollection = $this->createMock(Collection::class);
+        $mockCollection->method('getArrayCopy')->willReturn(['model1', 'model2']);
+
+        $this->camelCaseToDash->method('filter')->willReturn('students');
+        $this->pluralizeFilter->method('filter')->willReturn('Students');
+        $this->httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->with(HttpClientInterface::HTTP_METHOD_GET, $url, $options)
+            ->willReturn($responseData);
+
+        $result = $this->gateway->findAll($resourceType, $propertyFilters, $userTags);
+
+        $this->assertIsArray($result);
+        $student = $result['0'];
+        $this->assertInstanceOf(ModelBase::class, $student);
+        $this->assertSame('Student', $student->getResourceType());
+    }
+
+    /**
+     * @throws \Arbor\Query\Exception
+     * @throws Exception
+     * @throws \Arbor\Model\Exception
+     * @throws ServerErrorException
+     */
+    public function testFindAllWithEmptyFiltersReturnsArray()
+    {
+        $resourceType = 'Student';
+        $propertyFilters = [];
+        $userTags = [];
+
+        $mockCollection = $this->createMock(Collection::class);
+        $mockCollection->method('getArrayCopy')->willReturn([]);
+
+        $this->camelCaseToDash->method('filter')->willReturn('students');
+        $this->pluralizeFilter->method('filter')->willReturn('Students');
+
+        $result = $this->gateway->findAll($resourceType, $propertyFilters, $userTags);
+
+        $this->assertIsArray($result);
+        $this->assertEmpty($result);
     }
 }
